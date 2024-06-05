ec9df3d4ba6ea33d5edd37f4d795562d3de5e069
Darkzwer <52218993+Darkzwer@users.noreply.github.com>
2024-06-05 15:21:22 +0300

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "RegulaCommon",
+    products: [
+        .library(
+            name: "RegulaCommon",
+            targets: ["RegulaCommon"]),
+    ],
+    targets: [
+        .binaryTarget(name: "RegulaCommon", url: "https://pods.regulaforensics.com/RegulaCommon/7.2.487/RegulaCommon-7.2.487.zip", checksum: "79e401a8cfc0a16751479bccfca645709037e0afd6c78f653b765fc846ad0220"),
+    ]
+)
